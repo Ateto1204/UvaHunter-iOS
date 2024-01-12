f6dd4eaa44378360ce5454acaaec37f5f495321e
Ateto1202 <tonysu1204@gmail.com>
2024-01-12 16:44:54 +0800

    Update UvaHunter Now the program could get user id by entering user name, and it could get json format of unsolved problems by user id, and what the task to do is decode the json.

--- UvaHunter.swiftpm/ContentView.swift
+++ UvaHunter.swiftpm/ContentView.swift
@@ -3,30 +3,24 @@ import Foundation
 
 struct ContentView: View {
     
+    @State private var userName: String = ""
     @State private var userId: String = ""
     @State private var isButtonPressed: Bool = false
-    @State private var problems: [Int] = []
+    @State private var problems: String = ""
     @State private var name: String = ""
     
     var body: some View {
         VStack {
             HStack {
-                TextField("Enter the user id", text: $userId)
+                TextField("Enter the user name", text: $userName)
                     .padding()
                     .background(Color.gray)
                     .cornerRadius(6)
                     .padding()
                 Button {
-                    self.isButtonPressed = true
-                    getUserName(forUserId: userId) { userName in 
-                        if let userName = userName {
-                            self.name = userName
-                        } else {
-                            self.name = "null"
-                        }
-                        
-                    }
-//                    self.problems = getUnsolveProblem(forUserId: userId)
+                    print(userName)
+                    getUserId(userName: userName)
+                    getProblems(userId: userId)
                 } label: {
                     Image(systemName: "location.circle")
                         .resizable()
@@ -36,80 +30,50 @@ struct ContentView: View {
                 }
             }
             
-            Text(name)
-            
-//            if problems.count > 0 {
-//                List {
-//                    ForEach(problems.indices) { idx in 
-//                        Text("\(idx): \(problems[idx])")
-//                    }
+            Text(userId)
+            Text(problems)
+//            ScrollView {
+//                ForEach(problems.indices) { idx in 
+//                    Text(problems[idx])
 //                }
 //            }
         }
     }
     
-    func getUnsolveProblem(forUserId userId: String) -> [Int] {
-        let url = URL(string: "https://uhunt.onlinejudge.org/api/p/unsolved/\(userId)")!
-        var result: [Int] = []
+    func getProblems(userId: String) {
+        guard let url = URL(string: "https://uhunt.onlinejudge.org/api/p/unsolved/\(userId)") else {
+            return 
+        }
         
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in 
+        URLSession.shared.dataTask(with: url) { data, response, error in 
             if let error = error {
-                print("Error: \(error)")
+                print(error)
             } else if let data = data {
-                do {
-                    print("B")
-                    if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                        print("A")
-                        
-                        if let problems = jsonResponse["problems"] as? [[String: Any]] {
-                            let unsolvedProblems = problems.prefix(5)
-                            for problem in unsolvedProblems {
-                                if let problemId = problem["pnum"] as? Int {
-                                    result.append(problemId)
-                                }
-                            }
-                        }
+                if let problems = String(data: data, encoding: .utf8) {
+                    DispatchQueue.main.sync {
+                        self.problems = problems
                     }
-                } catch {
-                    print("Error parsing JSON: \(error)")
                 }
             }
-        }
-        print("\(result.count)")
-        task.progress.resume()
-        
-        return result
+        }.resume()
     }
     
-    func getUserName(forUserId userId: String, completion: @escaping (String?) -> Void) {
-        let apiUrl = "http://uhunt.onlinejudge.org/api/uname2uid/"
-        let url = URL(string: "\(apiUrl)\(userId)")!
-        
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print("Error: \(error)")
-                completion(nil)
-            } else if let data = data {
-                do {
-                    if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                        if let userName = jsonResponse["username"] as? String {
-                            completion(userName)
-                        } else {
-                            print("Invalid JSON format: Missing 'username' key")
-                            completion(nil)
-                        }
-                    } else {
-                        print("Failed to parse JSON")
-                        completion(nil)
-                    }
-                } catch {
-                    print("Error parsing JSON: \(error)")
-                    completion(nil)
-                }
-            }
+    func getUserId(userName: String) {
+        guard let url = URL(string: "https://uhunt.onlinejudge.org/api/uname2uid/\(userName)") else {
+            return 
         }
         
-        task.resume()
+        URLSession.shared.dataTask(with: url) { data, response, error in 
+            if let error = error {
+                print(error)
+            } else if let data = data {
+                if let id = String(data: data, encoding: .utf8) {
+                    DispatchQueue.main.sync {
+                        self.userId = id
+                    }
+                }
+            }
+        }.resume()
     }
     
 }
